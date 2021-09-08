abdcd7d81bc18809a0a68bed0fca97cba593466a
NatsukiPrayer <aloneandtired@yandex.ru>
2021-09-08 13:58:52 +0300

    feature/add/angle_between_vectors

--- classes/Vector.py
+++ classes/Vector.py
@@ -1,7 +1,6 @@
 from classes.Point import Point
 import numpy as np
 
-
 class Vector:
     end: Point
 
@@ -26,3 +25,7 @@ class Vector:
     def __add__(self, other) -> "Vector":
         return Vector(Point(self.end.values + other.values))
 
+    def angle_between(self, v2) -> float:
+        v1_u = self.normalize()
+        v2_u = v2.normalize()
+        return np.arccos(np.clip(np.dot(v1_u.end.values, v2_u.end.values), -1.0, 1.0))
