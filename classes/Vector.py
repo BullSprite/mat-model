73b4fbfe8207cf1053e848065d962e017e860aac
NatsukiPrayer <47858718+NatsukiPrayer@users.noreply.github.com>
2021-09-08 14:34:14 +0300

    feature/angle_between_vectors (#9)
    * feature/angle_between_vectors/add_function
    
    * feature/angle_between_vectors/changed_angle_to_cos_angle

--- classes/Vector.py
+++ classes/Vector.py
@@ -26,3 +26,7 @@ class Vector:
     def __add__(self, other) -> "Vector":
         return Vector(Point(self.end.values + other.values))
 
+    def cos_angle_between(self, v2) -> float:
+        v1_u = self.normalize().end.values
+        v2_u = v2.normalize().end.values
+        return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
