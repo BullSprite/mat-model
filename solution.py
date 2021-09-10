32c74a1698c4a100836723facfb906782a1e678f
NatsukiPrayer <aloneandtired@yandex.ru>
2021-09-10 18:19:16 +0300

    feature/solution/add_standalone_functions

--- solution.py
+++ solution.py
@@ -2,28 +2,35 @@ from classes.Triangel import Triangle
 from classes.Frame import Frame
 from classes.Point import Point
 from classes.Line import Line
+from classes.Vector import Vector
 import numpy as np
 from math import acos, log, tan
+
+
+def angle_calculation(v1: Vector, v2: Vector, height: float, triangle: Triangle):
+    if v1.len() * v2.len() * v1.to_vector().cos_angle_between(v2.to_vector()) > 0:
+        return (acos(height / triangle.lines[1].len()))
+    else:
+        return (-acos(height / triangle.lines[1].len()))
+
+
+def integral(line: Line, op):
+    triangle = Triangle(line.start, line.end, op)
+    height = triangle.get_height(op.name)
+    intersect = triangle.get_height_intersect(op.name)
+    HA = Line(intersect, triangle.points[0])
+    HB = Line(intersect, triangle.points[1])
+    AB = Line(triangle.points[0], triangle.points[1])
+    amin = angle_calculation(AB, HB, height, triangle)
+    amax = angle_calculation(AB, HA, height, triangle)
+    return (height * (log(abs((1 + tan(amax / 2)) / (1 - tan(amax / 2)))) - log(
+        abs((1 + tan(amin / 2)) / (1 - tan(amin / 2))))))
+
 
 def solution(points: list[np.ndarray], point_O: np.ndarray, num_f=1, num_p=1):
     integ = 0
     op = Point(point_O, name='O')
-    p = [Point(p,name=name) for p, name in zip(points, 'ABCD')]
+    p = [Point(p, name=name) for p, name in zip(points, 'ABCD')]
     Sol_Frame = Frame(p[0], p[1], p[2], p[3])
-    for line in Sol_Frame.lines_iter():
-        triangle = Triangle(line.start, line.end, op)
-        height = triangle.get_height(op.name)
-        intersect = triangle.get_height_intersect(op.name)
-        HA = Line(intersect, triangle.points[0])
-        HB = Line(intersect, triangle.points[1])
-        AB = Line(triangle.points[0], triangle.points[1])
-        if AB.len()*HB.len()*AB.to_vector().cos_angle_between(HB.to_vector()) > 0:
-            amin = acos(height/triangle.lines[1].len())
-        else:
-            amin = -acos(height/triangle.lines[1].len())
-        if AB.len()*HA.len()*AB.to_vector().cos_angle_between(HA.to_vector()) > 0:
-            amax = acos(height/triangle.lines[2].len())
-        else:
-            amax = -acos(height/triangle.lines[2].len())
-        integ += height*(log(abs((1+tan(amax/2))/(1-tan(amax/2))))-log(abs((1+tan(amin/2))/(1-tan(amin/2)))))
-    return integ
+    return sum(integral(line, op) for line in Sol_Frame.lines_iter())
+
