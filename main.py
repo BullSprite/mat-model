515867b1d5da4d624e34d0c41861ee77b3d2a3fd
NatsukiPrayer <47858718+NatsukiPrayer@users.noreply.github.com>
2021-09-10 19:54:56 +0300

    Feature/solution (#10)
    * feature/frame_class/add_frame_class
    
    * feature/frame_class/changed_iterator
    
    * feature/frame_class/fix_lines
    
    * feature/solution/add_solution
    
    * feature/solution/add_standalone_functions
    
    * feature/solution/add_high_log

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from solution import solution
+import numpy as np
+
+
 def main():
-    pass
+    print(solution([np.array([2,2,0]),
+              np.array([4,2,0]),
+              np.array([4,4,0]),
+              np.array([2,4,0])],
+             np.array([3,3,0])))
 
 if __name__ == "__main__":
     main()
