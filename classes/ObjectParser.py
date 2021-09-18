1e3e5f03a4a364cce45d6868b3bc62cf47d2aada
NatsukiPrayer <aloneandtired@yandex.ru>
2021-09-18 16:53:57 +0300

    feauture/object_parser/dict_to_list

--- classes/ObjectParser.py
+++ classes/ObjectParser.py
@@ -2,26 +2,26 @@ from classes.Frame import Frame
 from classes.Point import Point
 
 
-class Object_parser:
+class ObjectParser:
 
     def __init__(self, file):
-        parsed_dict = {}
+        self.parsed_list = []
         with open(file, 'r') as text:
             object_num = int(text.readline())
-            for i in range(0, object_num):
-                parsed_dict['object' + str(i)] = {}
+            for object in range(0, object_num):
+                self.parsed_list.append([])
                 modules_num = int(text.readline())
-                for j in range(0, modules_num):
-                    parsed_dict['object' + str(i)]['module' + str(j)] = []
+                for module in range(0, modules_num):
+                    self.parsed_list[object].append([])
                     text.readline()
                     lines_num = int(text.readline())
                     text.readline()
-                    for k in range(0, lines_num):
+                    for lines in range(0, lines_num):
                         points = []
                         line = text.readline().strip().split()
                         for start, name in zip(range(0, 12, 3), 'ABCD'):
                             points.append(Point([x for x in map(float, line[start: start + 3])],
-                                                name=name + str(i) + str(j) + str(k)))
-                        parsed_dict['object' + str(i)]['module' + str(j)].append(
+                                                name=name + str(object) + str(module) + str(lines)))
+                        self.parsed_list[object][module].append(
                             Frame(points[0], points[1], points[2], points[3]))
-        self.parsed_dict = parsed_dict
+
