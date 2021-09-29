0bd4661b2ad1c523cfe9e6004b09570e9c9e3016
NatsukiPrayer <aloneandtired@yandex.ru>
2021-09-30 01:57:44 +0300

    feature/Scene/big

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from classes.Scene import Scene
 from tests import test
+from classes.Frame import Frame
+from classes.Point import Point
+import numpy as np
+
 
 
 def main():
-    test()
+    out = Scene('figures/sphere.dat')
+    #print(out.parsed_list[1][3][2]==out.parsed_list[0][0][2])
+    #print(solution(out.parsed_list[0][0]))
+    #gen = next(out)
+    print(out.collaction(0))
 
 
 if __name__ == "__main__":
